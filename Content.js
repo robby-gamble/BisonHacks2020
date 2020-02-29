b4fd595cb980d28910897e3c149245ea9eba3694
Procrascat <khalil.luke@gmail.com>
2020-02-29 06:45:47 -0500

    This is getting messy

--- Content.js
+++ Content.js
@@ -1,14 +1,26 @@
 var Word = window.location.href;
-document.write(Word);
 
+
+var ALtArtTwo;
+
+
+        /*
 switch(Word) {
-    case "https://www.politico.com/news/2020/02/28/trump-south-carolina-rally-coronavirus-118269":
-
+    
+    case "http://127.0.0.1:5500/BisonHacks2020/Content.html":
+       // document.getElementById('sauce').innerHTML = "https://apnews.com/ebc90501a013f52d792e72069bd4ad7f";
+        
+    case "https://apnews.com/ebc90501a013f52d792e72069bd4ad7f":
+        
         break;
 
+    case "https://www.foxnews.com/politics/trump-rallies-in-north-charleston-on-eve-of-south-carolina-primary":
+        
+        break;
+    
 }
-
-
+*/
+}
 
 /*"content_scripts": {
         "matches": ["<all_urls>"],
