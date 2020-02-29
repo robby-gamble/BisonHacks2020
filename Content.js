6725547b40803a2ee590ec900b6ccbd561c9e040
Procrascat <khalil.luke@gmail.com>
2020-02-29 04:20:44 -0500

    started making switch statement

--- Content.js
+++ Content.js
@@ -1,5 +1,15 @@
-var Word = "Khalil";
+var Word = window.location.href;
 document.write(Word);
+
+switch(Word) {
+    case "https://www.politico.com/news/2020/02/28/trump-south-carolina-rally-coronavirus-118269":
+
+        break;
+
+}
+
+
+
 /*"content_scripts": {
         "matches": ["<all_urls>"],
         "js": ["BiasBehavior.js"]
